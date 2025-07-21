0c911e4c3d2e38d25287f275763dd8b95b19135c
mayank3dwork <145151809+mayank3dwork@users.noreply.github.com>
2025-07-22 00:25:14 +0530

    Add files via upload

---
+++ ins-kadamba-_junior-sailors-renders/app-files/data.js
@@ -0,0 +1,239 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-agarwal-interiors_entrance",
+      "name": "Agarwal Interiors_entrance.",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6423301718984789,
+          "pitch": 0.02015486294268598,
+          "rotation": 0,
+          "target": "1-agarwal-interiors_render-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-agarwal-interiors_render-02",
+      "name": "Agarwal Interiors_Render 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7429154816686605,
+          "pitch": 0.06648997411099344,
+          "rotation": 0,
+          "target": "2-agarwal-interiors_render-01"
+        },
+        {
+          "yaw": 1.1547679282458425,
+          "pitch": 0.07511806701258195,
+          "rotation": 0,
+          "target": "3-agarwal-interiors_render-03"
+        },
+        {
+          "yaw": -0.7984222121544917,
+          "pitch": 0.012556557774763277,
+          "rotation": 0,
+          "target": "0-agarwal-interiors_entrance"
+        },
+        {
+          "yaw": -0.3761371956194761,
+          "pitch": -0.0005508483805805042,
+          "rotation": 0,
+          "target": "5-agarwal-interiors_render_kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-agarwal-interiors_render-01",
+      "name": "Agarwal Interiors_Render 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.19873014827962265,
+          "pitch": 0.0617666629372664,
+          "rotation": 0,
+          "target": "1-agarwal-interiors_render-02"
+        },
+        {
+          "yaw": 0.5074425864987013,
+          "pitch": 0.06284470826359367,
+          "rotation": 0,
+          "target": "4-agarwal-interiors_render-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-agarwal-interiors_render-03",
+      "name": "Agarwal Interiors_Render 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14209155999776968,
+          "pitch": 0.08634372013283631,
+          "rotation": 0,
+          "target": "1-agarwal-interiors_render-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-agarwal-interiors_render-01",
+      "name": "Agarwal Interiors_Render 01.",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6503793998954936,
+          "pitch": 0.06913528807756286,
+          "rotation": 0,
+          "target": "2-agarwal-interiors_render-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-agarwal-interiors_render_kitchen",
+      "name": "Agarwal-Interiors_Render_kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0832079573963416,
+          "pitch": 0.013765951647560115,
+          "rotation": 0,
+          "target": "1-agarwal-interiors_render-02"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "ins-kadamba-_junior-sailors-renders",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
